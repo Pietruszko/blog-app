288cbb88d64611456d078cb40a8da1fada382976
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-06 11:16:42 +0200

    feat: add view for listing posts by tag, add path to it in urls and revert to function based view for post list view

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from .models import Post
+from taggit.models import Tag
 from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
@@ -12,8 +13,12 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -27,7 +32,10 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {
+            'posts': posts,
+            'tag': tag
+        }
     )
 
 def post_detail(request, year, month, day, post):
