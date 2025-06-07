0078c59876c95cd6fa89e71e8687d470e045b31a
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-07 12:30:54 +0200

    feat: create post search view that uses search form to search for user queries with search vetor for title and renders search template

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,10 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 from .models import Post
 from taggit.models import Tag
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -136,5 +137,31 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                )
+                .filter(search=query)
+            )
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
